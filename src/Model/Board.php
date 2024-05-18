c1d9832fce42cea1bf1585f9b56887906d5265df
Galina Leshchenko <ipz222_lgo@student.ztu.edu.ua>
2024-05-18 15:54:40 +0300

    +getSymbol method

--- src/Model/Board.php
+++ src/Model/Board.php
@@ -1,6 +1,32 @@
 <?php
+// The playing field in the form of a two-dimensional array
 
-class Board
-{
+class Board {
+    private $board;
+
+    public function __construct() {
+        $this->board = array_fill(0, 3, array_fill(0, 3, ' '));
+    }
+
+    public function display() {
+        foreach ($this->board as $row) {
+            echo implode('|', $row) . PHP_EOL;
+            echo "-----" . PHP_EOL;
+        }
+    }
+
+    public function mark($row, $col, $symbol) {
+        if ($this->board[$row][$col] === ' ') {
+            $this->board[$row][$col] = $symbol;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function getSymbol($row, $col) {
+        return $this->board[$row][$col];
+    }
 
 }
+?>
